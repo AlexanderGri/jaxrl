832b867fa35fb479168034e257e2af62765a6e25
AlexanderGri <grishin.alexgri@yandex.ru>
2022-08-24 09:45:09 +0200

    Fix qr

--- jaxrl/extra_utils.py
+++ jaxrl/extra_utils.py
@@ -1,14 +1,14 @@
 from jax import dtypes, jit, lax, random
 import jax.numpy as jnp
 from jax._src.util import prod
-from jax.ops import index, index_add, index_update
 from jax.numpy.linalg import norm
 
 import numpy as np
 
 
 @jit
-def factor_mgs(A):
+def qr(A):
+    A = A.T
     n, m = A.shape
     # n rows
     # m dimension
@@ -20,19 +20,19 @@ def factor_mgs(A):
         # fill the k-th diagonal entry in R
         atom = A[k]
         norm_a = norm(atom)
-        R = index_update(R, index[k, k], norm_a)
+        R = R.at[k, k].set(norm_a)
         # Initialize the k-th vector in Q
         q = atom / norm_a
-        Q = index_update(Q, index[k], q)
+        Q = Q.at[k].set(q)
         # Compute the inner product of new q vector with each of the remaining rows in A
         products = A[k+1:n, :] @ q.T
         # Place in k-th column of R
-        R = index_update(R, index[k+1:n, k], products)
+        R = R.at[k+1:n, k].set(products)
         # Subtract the contribution of previous q vector from all remaining rows of A.
         rr = R[k+1:n, k:k+1]
-        update =  -rr @ jnp.expand_dims(q, 0)
-        A = index_add(A, index[k+1:n], update)
-    return Q, R
+        update = -rr @ jnp.expand_dims(q, 0)
+        A = A.at[k+1:n].add(update)
+    return Q.T, R.T
 
 
 def orthogonal(scale=1.0, column_axis=-1, dtype=jnp.float_):
@@ -49,9 +49,7 @@ def orthogonal(scale=1.0, column_axis=-1, dtype=jnp.float_):
     n_rows, n_cols = prod(shape) // shape[column_axis], shape[column_axis]
     matrix_shape = (n_cols, n_rows) if n_rows < n_cols else (n_rows, n_cols)
     A = random.normal(key, matrix_shape, dtype)
-    Q, R = factor_mgs(jnp.transpose(A))
-    Q = -jnp.transpose(Q)
-    R = -jnp.transpose(R)
+    Q, R = factor_mgs(A)
     diag_sign = lax.broadcast_to_rank(jnp.sign(jnp.diag(R)), rank=Q.ndim)
     Q *= diag_sign # needed for a uniform distribution
     if n_rows < n_cols: Q = Q.T
